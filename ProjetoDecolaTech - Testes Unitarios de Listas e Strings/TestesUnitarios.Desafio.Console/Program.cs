0312dd84ed08f2a8197fbe4b7896efe30db873b8
Petersonhenry <107315053+PetersonPHC@users.noreply.github.com>
2024-01-09 18:52:36 -0300

    Add files via upload

---
+++ ProjetoDecolaTech - Testes Unitarios de Listas e Strings/TestesUnitarios.Desafio.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using TestesUnitarios.Desafio.Console.Services;
+
+ValidacoesLista validacoes = new ValidacoesLista();
+
+var lista = new List<int> { 5, -1, -8, 9 };
+var resultadoEsperado = new List<int> { 5, 9 };
+
+// Act
+var resultado = validacoes.ListaContemDeterminadoNumero(lista, 10);
+Console.WriteLine(resultado);
+/*
+foreach (var i in resultado)
+{
+
+    Console.WriteLine(i);
+}*/
